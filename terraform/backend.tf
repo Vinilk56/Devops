33d576e418ca7574e1d0fb479c6ef04b6d2eda02
Vinilk56 <104364641+Vinilk56@users.noreply.github.com>
2026-03-17 13:43:11 +0530

    Update backend.tf
    ok

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "tfstate_rg" {
 
 # Storage Account for backend
 resource "azurerm_storage_account" "tfstate_sa" {
-  name                     = "tfstateaccount1"   # must be globally unique
+  name                     = "tfstateaccount"   # must be globally unique
   resource_group_name      = azurerm_resource_group.tfstate_rg.name
   location                 = azurerm_resource_group.tfstate_rg.location
   account_tier             = "Standard"

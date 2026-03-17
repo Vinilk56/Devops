8a15e3413edea2223ae03ff4c7f806981ed124e7
Vinilk56 <104364641+Vinilk56@users.noreply.github.com>
2026-03-17 13:48:30 +0530

    Update backend.tf
    Corrected code

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -8,9 +8,16 @@ resource "azurerm_resource_group" "tfstate_rg" {
   location = "East US"
 }
 
+# Random suffix to ensure globally unique storage account name
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
 # Storage Account for backend
 resource "azurerm_storage_account" "tfstate_sa" {
-  name                     = "tfstateaccount"   # must be globally unique
+  name                     = "tfstate${random_string.suffix.result}" # must be globally unique
   resource_group_name      = azurerm_resource_group.tfstate_rg.name
   location                 = azurerm_resource_group.tfstate_rg.location
   account_tier             = "Standard"
@@ -20,6 +27,6 @@ resource "azurerm_storage_account" "tfstate_sa" {
 # Storage Container for backend
 resource "azurerm_storage_container" "tfstate_container" {
   name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate_sa.name
+  storage_account_id    = azurerm_storage_account.tfstate_sa.id
   container_access_type = "private"
 }

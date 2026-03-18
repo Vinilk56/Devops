5e5acf9679f3dee9d785eb765ef05db0fc84515b
Vinilk56 <104364641+Vinilk56@users.noreply.github.com>
2026-03-19 00:06:03 +0530

    Create vmbuild.tf
    New VM build

---
+++ terraform/vmbuild.tf
@@ -0,0 +1,69 @@
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "linux-vm-rg"
+  location = "East US"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "example" {
+  name                = "linux-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Subnet
+resource "azurerm_subnet" "example" {
+  name                 = "linux-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Network Interface (no public IP)
+resource "azurerm_network_interface" "example" {
+  name                = "linux-vm-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "linux-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  # SSH key authentication
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")   # path to your public key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20.04-LTS"
+    version   = "latest"
+  }
+}

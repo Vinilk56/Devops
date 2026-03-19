ef59f52ce17f798bdaa0c6215ebd64067bc9cc9f
Vinilk56 <104364641+Vinilk56@users.noreply.github.com>
2026-03-19 11:44:24 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" { type = string }
+variable "location"            { type = string }
+variable "vm_name"             { type = string }
+variable "vm_size"             { type = string default = "Standard_B1s" }
+variable "admin_username"      { type = string default = "azureuser" }
+variable "ssh_public_key"      { type = string }
+
+variable "vnet_name"           { type = string default = "linux-vnet" }
+variable "vnet_address_space"  { type = string default = "10.0.0.0/16" }
+variable "subnet_name"         { type = string default = "linux-subnet" }
+variable "subnet_prefix"       { type = string default = "10.0.1.0/24" }
+
+variable "image_publisher"     { type = string default = "Canonical" }
+variable "image_offer"         { type = string default = "UbuntuServer" }
+variable "image_sku"           { type = string default = "20.04-LTS" }
